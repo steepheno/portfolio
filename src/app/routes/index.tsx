f8bcbad4a3d79ed2a0b47ae6d74cd7de6494a136
Joonsun Ryu <rjs7289@gmail.com>
2026-03-16 21:56:24 +0900

    feat: 프로젝트 상세 페이지 라우팅 생성

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from 'react-router-dom';
 import MainPage from '@/pages/mainPage/MainPage';
 import MainLayout from '@/shared/ui/MainLayout';
 import Resume from '@/pages/resume/Resume';
+import DoroLaw from '@/pages/projects/Dorolaw';
+import ChoiceAndAppear from '@/pages/projects/ChoiceAndAppear';
+import DevPilot from '@/pages/projects/DevPilot';
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +18,23 @@ export const router = createBrowserRouter([
       {
         path: '/resume',
         element: <Resume />,
+      },
+      {
+        path: '/projects',
+        children: [
+          {
+            path: 'choice-and-appear',
+            element: <ChoiceAndAppear />,
+          },
+          {
+            path: 'dorolaw',
+            element: <DoroLaw />,
+          },
+          {
+            path: 'devpilot',
+            element: <DevPilot />,
+          },
+        ],
       },
     ],
   },
