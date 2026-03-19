4df503b429d33d1599abd75c22adb5ccbc5775a6
Joonsun Ryu <rjs7289@gmail.com>
2026-03-19 21:10:19 +0900

    rename: SavePdf.tsx -> SavePdfDetail.tsx 파일명 수정

---
+++ src/features/dorolaw/SavePdfDetail.tsx
@@ -0,0 +1,11 @@
+import SourceCodeCard from '@/shared/ui/SourceCodeCard';
+
+export default function SavePdfDetail() {
+  return (
+    <>
+      <p>PDF 저장 기능 구현</p>
+
+      <SourceCodeCard fileName="axiosInstance.js" />
+    </>
+  );
+}
